32723c7971ad993e86e89e6d3c4e7578e3cd9305
sebranly <sebranly.pinf@gmail.com>
2020-09-06 21:18:54 +0200

    chore: bump version

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test <code>2</code>
+          Test <code>3</code>
         </p>
         <a
           className="App-link"
