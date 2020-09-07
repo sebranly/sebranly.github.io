37080e4e5c886585fcdb505357cbd9ff1eaee5d6
sebranly <sebranly.pinf@gmail.com>
2020-09-07 22:07:00 +0200

    feat: add really basic website structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test <code>4</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Sebastien</h1>
+      <h2>Programming</h2>
+      <div className="menu">
+        <ul className="choices">
+          <li className="choice">Choice 1</li>
+          <li className="choice">Choice 2</li>
+          <li className="choice">Choice 3</li>
+          <li className="choice">Choice 4</li>
+        </ul>
+      </div>
+      <div className="body">
+        <div className="block-2">
+          <div className="article">
+            <h3>Something</h3>
+            <p>One paragraph</p>
+            <p>Another one</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="article">
+            <h3>Something else</h3>
+            <p>Another one</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>One paragraph</p>
+          </div>
+        </div>
+        <div className="block-1">
+          <div className="section">
+            <h3>Some section</h3>
+            <p>Short description</p>
+            <p>One paragraph</p>
+          </div>
+          <div className="section">
+            <h3>Some other section</h3>
+            <p>Very short description</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer">Created in 2020</div>
     </div>
   );
 }
